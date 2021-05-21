64103d7abce108e849d01833f3d4ea12f3a4c46e
Ankita <ankitakalangutkar90@gmail.com>
2021-05-21 14:18:11 +0200

    Initial setup

---
+++ Wander/Wander-bridging-header.h
@@ -0,0 +1,8 @@
+//
+//  Wander-bridging-header.h
+//  Wander
+//
+//  Created by Ankita on 21.05.21.
+//
+
+#import<FlickrKit/FlickrKit.h>
---
+++ Wander/Wander/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wander
+//
+//  Created by Ankita on 21.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
